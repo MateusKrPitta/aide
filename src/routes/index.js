eafdbebede555405e95239e71301c9ac9aa1449b
Mateus Kronbauer Pitta <contato.mateuspitta@gmail.com>
2025-05-23 13:40:17 -0400

    fix:correção

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import Clientes from "../pages/cadastro/clientes/index.js";
 import Atendimentos from "../pages/atendimentos/index.js";
 import PalestrasCursos from "../pages/palestra-cursos/index.js";
 import Financeiro from "../pages/financeiro/index.js";
+import Prestadores from "../pages/cadastro/prestadores/index.js";
 
 const AppRoutes = () => {
   return (
@@ -17,6 +18,7 @@ const AppRoutes = () => {
       <Route path="/Atendimentos" element={<Atendimentos />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/cadastro/usuario" element={<Usuario />} />
+      <Route path="/cadastro/prestadores" element={<Prestadores />} />
       <Route path="/cadastro/servicos" element={<Servicos />} />
       <Route path="/cadastro/clientes" element={<Clientes />} />
       <Route path="/palestras-cursos" element={<PalestrasCursos />} />
