46dc14402593bb47ee2538f4728f2890b585ff86
MateusKrPitta <contato.mateuspitta@gmail.com>
2026-09-24 10:12:00 -0300

    fix: remover header incorreto de origin no client axios

--- src/service/url.js
+++ src/service/url.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-const API_URL = `${process.env.REACT_APP_BASE_API}`;
+const API_URL = `${process.env.REACT_APP_BASE_API || ""}`;
 const httpsInstance = () => {
   const httpsAuthenticated = axios.create({
     baseURL: API_URL,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
   });
 
   httpsAuthenticated.interceptors.request.use(
